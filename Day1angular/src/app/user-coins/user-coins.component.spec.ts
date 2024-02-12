9e8735269da5c102b61894e70605469256c2d21e
Rahulgogu1 <156913808+Rahulgogu1@users.noreply.github.com>
2024-02-12 21:06:07 +0530

    Add files via upload

---
+++ Day1angular/src/app/user-coins/user-coins.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserCoinsComponent } from './user-coins.component';
+
+describe('UserCoinsComponent', () => {
+  let component: UserCoinsComponent;
+  let fixture: ComponentFixture<UserCoinsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UserCoinsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UserCoinsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Day1angular/src/app/user-coins/user-coins.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-coins',
+  templateUrl: './user-coins.component.html',
+  styleUrls: ['./user-coins.component.css']
+})
+export class UserCoinsComponent {
+
+}
+
